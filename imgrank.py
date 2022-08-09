7842246d2f8ace8146c8ad76561cc43061f965ee
cosmicpb <pbaldacimjr@gmail.com>
2022-08-09 15:01:34 -0300

    Update imgrank.py

--- imgrank.py
+++ imgrank.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import requests
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -11,7 +12,10 @@ driver.implicitly_wait(10)
 images = driver.find_elements(By.TAG_NAME, 'img')
 
 for img in images:
-    print(img.accessible_name)
-    print(img.get_attribute('src'))
+    
+    imgsource = img.get_attribute('src')
+    r = requests.get(imgsource)
+    print(imgsource)
+    print(r.headers['Content-length'])
 
     
