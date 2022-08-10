1eaff17d37ab438687e780c157588f6624e6d3af
cosmicpb <pbaldacimjr@gmail.com>
2022-08-10 15:39:35 -0300

    Update imgrank_linux.py

--- imgrank_linux.py
+++ imgrank_linux.py
@@ -7,6 +7,12 @@ import requests
 import click
 import time
 
+class obj:
+    def __init__(self, url, size):
+        self.url = url
+        self.size = size
+
+
 @click.command()
 @click.option('-h', required=True, type=str)
 @click.option('-t', required=True, type=str)
@@ -24,23 +30,33 @@ def func(h, t):
     time.sleep(int(t))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     images = driver.find_elements(By.TAG_NAME, 'img')
-    
+    objs = []
     for img in images:
         
         imgsource = img.get_attribute('src')
         if(imgsource):
             r = requests.get(imgsource)
-            print(imgsource)
+            #print(imgsource)
             el = r.headers.get('Content-length')
             if(el.isdigit()):
-                
+
                 el = int(el)/1024
+                fel = "{:.2f}".format(el)
+
+                if(obj(imgsource, float(fel)) not in objs):
+                    objs.append(obj(imgsource,float(fel)))                
                 
-                print("Image Size: %.2f kB" % el)
-            else:
-                print('There is no Content-Length in .head')
+                #print("Image Size: %.2f kB" % el)
+            #else:
+                #print('There is no Content-Length in .head')
 
-
+    objs.sort(key=lambda x: x.size)
+    for ent in objs:
+        print(getattr(ent, 'url'))
+        print(getattr(ent, 'size'))
+        
+       
+        
 if __name__ == '__main__':
     
     func()
