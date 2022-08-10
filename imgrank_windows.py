72f9b4ba97afa62afa6be4bfe8c141a8ef5cb766
cosmicpb <pbaldacimjr@gmail.com>
2022-08-10 15:37:56 -0300

    Update imgrank_windows.py

--- imgrank_windows.py
+++ imgrank_windows.py
@@ -6,33 +6,50 @@ import requests
 import click
 import time
 
+class obj:
+    def __init__(self, url, size):
+        self.url = url
+        self.size = size
+
+
 @click.command()
 @click.option('-h', required=True, type=str)
 @click.option('-t', required=True, type=str)
+
 
 def func(h, t):
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     driver.get(h)
-    time.sleep(t)
+    time.sleep(int(t))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     images = driver.find_elements(By.TAG_NAME, 'img')
-    
+    objs = []
     for img in images:
         
         imgsource = img.get_attribute('src')
         if(imgsource):
             r = requests.get(imgsource)
-            print(imgsource)
+            #print(imgsource)
             el = r.headers.get('Content-length')
             if(el.isdigit()):
-                
+
                 el = int(el)/1024
+                fel = "{:.2f}".format(el)
+
+                if(obj(imgsource, float(fel)) not in objs):
+                    objs.append(obj(imgsource,float(fel)))                
                 
-                print("Image Size: %.2f kB" % el)
-            else:
-                print('There is no Content-Length in .head')
+                #print("Image Size: %.2f kB" % el)
+            #else:
+                #print('There is no Content-Length in .head')
+
+    objs.sort(key=lambda x: x.size)
+    for ent in objs:
+        print(getattr(ent, 'url'))
+        print(getattr(ent, 'size'))
+        
        
         
 
