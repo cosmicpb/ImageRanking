210cc5f1fbb6e9042b69e902ae9b0a54c6d4ea26
cosmicpb <pbaldacimjr@gmail.com>
2022-08-11 10:18:26 -0300

    Update imgrank.py

--- imgrank.py
+++ imgrank.py
@@ -3,14 +3,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from datetime import datetime
 import requests
 import click
 import time
 
 class obj:
-    def __init__(self, url, size):
+    def __init__(self, url, size, time):
         self.url = url
         self.size = size
+        self.time = time
 
 
 @click.command()
@@ -25,26 +27,32 @@ def func(h, t):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    
     
     driver.get(h)
     time.sleep(int(t))
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     images = driver.find_elements(By.TAG_NAME, 'img')
     objs = []
+    imgsum = 0
     for img in images:
         
         imgsource = img.get_attribute('src')
         if(imgsource):
+
             r = requests.get(imgsource)
             #print(imgsource)
-            el = r.headers.get('Content-length')
+            el = str(r.headers.get('Content-length'))
             if(el.isdigit()):
 
                 el = int(el)/1024
                 fel = "{:.2f}".format(el)
-
-                if(obj(imgsource, float(fel)) not in objs):
-                    objs.append(obj(imgsource,float(fel)))                
+                now = datetime.now()
+                tmnow = now.strftime("%d_%m_%Y.%H_%M_%S")
+                imgsum = imgsum + el
+                if(obj(imgsource, float(fel), tmnow) not in objs):
+                    objs.append(obj(imgsource,float(fel), tmnow))                
                 
                 #print("Image Size: %.2f kB" % el)
             #else:
@@ -52,8 +60,11 @@ def func(h, t):
 
     objs.sort(key=lambda x: x.size)
     for ent in objs:
+        print(str(getattr(ent, 'time')))
         print(getattr(ent, 'url'))
-        print(getattr(ent, 'size'))
+        print(str(getattr(ent, 'size')) + ' kB')
+    imgsum_f = "{:.2f}".format(imgsum)
+    print('Tamanho total das imagens na página: ' + str(imgsum_f) + ' kB')
         
        
         
